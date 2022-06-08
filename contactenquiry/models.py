838347beb6086fe5d69e2b56c7d018a72b76f08a
Narin Ahir <ahirnm266@gmail.com>
2022-06-08 20:51:05 +0530

    updated

--- contactenquiry/models.py
+++ contactenquiry/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from email.policy import default
 from django.db import models
-from tinymce.models import HTMLField
 
 
 class contactenquiry(models.Model):
     contact_name=models.CharField(max_length=50)
     contact_email=models.CharField(max_length=50)
     contact_phone=models.CharField(max_length=10)
-    contact_messege=HTMLField()
+    contact_messege=models.TextField()
 
 # Create your models here.
